903c700fa0b7bbc66a5969e391d7d0042339a159
Waysman Silva <waysman@gmail.com>
2017-08-03 12:27:57 -0300

    fix security problem, if back the page after logout can see informations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_cache_buster
+
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
     produtos_url
   end
 
+  def set_cache_buster
+   response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+   response.headers["Pragma"] = "no-cache"
+   response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,9 +12,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   def destroy
+     super
+     Rails.cache.clear
+   end
 
   # protected
 
